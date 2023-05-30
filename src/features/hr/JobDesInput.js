a2dfcdd67296f2f00dabd144701bf9cb20fc2f7c
minionsrahat <rahatuddin786@gmail.com>
2023-05-30 23:34:15 +0600

    Conditions Added

--- src/features/hr/JobDesInput.js
+++ src/features/hr/JobDesInput.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import ErrorText from '../../components/Typography/ErrorText';
 
-const JobDesInput = ({ handleSubmit, textRef }) => {
+const JobDesInput = ({ handleSubmit, textRef ,errorText}) => {
     return (
         <>
             <form onSubmit={handleSubmit} className='py-6'>
                 <div className="container items-center justify-center flex">
-                <textarea className="textarea textarea-primary w-1/2" ref={textRef} placeholder="Bio"></textarea>
+                <textarea className="textarea textarea-primary w-1/2" ref={textRef} placeholder="Job Description"></textarea>
                     <button type="submit" className='btn ml-3'>Submit</button>
                 </div>
+                <ErrorText>{errorText}</ErrorText>
             </form>
         </>
     );
--- src/features/hr/SortResume.js
+++ src/features/hr/SortResume.js
@@ -14,27 +14,58 @@ const SortResume = () => {
     const [selectedFile, setSelectedFile] = useState(null);
     const [resumes, setResumes] = useState([])
     const [sortedResumes, setSortedResumes] = useState([])
+    const [error, setErrorMsg] = useState('')
 
 
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     };
+    // Helper function to count words in a string
+    const countWords = (text) => {
+        const words = text.trim().split(/\s+/);
+        return words.length;
+    };
 
-    const handleSortResume=async(event)=>{
+    const handleSortResume = async (event) => {
         event.preventDefault();
-        let response;
-        const formData = new FormData();
-        formData.append('job_desc', jod_des_text.current.value);
-        formData.append('resume', JSON.stringify(resumes));
-        response = await axios.post('http://127.0.0.1:5000/sort_resumes', formData, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
 
-        const { sorted_resumes } = response.data
-        setSortedResumes(sorted_resumes)
-    }
+        const jobDescription = jod_des_text.current.value;
+
+        if (countWords(jobDescription) < 50) {
+            // Display an error message
+            setErrorMsg('Job description must have more than 100 words')
+        }
+
+        else {
+            setErrorMsg('')
+            const formData = new FormData();
+            formData.append('job_desc', jobDescription);
+            formData.append('resume', JSON.stringify(resumes));
+
+            try {
+                const response = await axios.post('http://127.0.0.1:5000/sort_resumes', formData, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                const { error, sorted_resumes } = response.data;
+                if (error) {
+                    setErrorMsg(error)
+                } else {
+                    setSortedResumes(sorted_resumes);
+                }
+
+            } catch (error) {
+                // Handle error
+                console.error(error);
+            }
+
+        }
+
+    };
+
+
 
     const handleResumeExceelSheetSubmit = async (event) => {
         event.preventDefault();
@@ -65,18 +96,18 @@ const SortResume = () => {
                 fileInputRef={fileInputRef}
                 handleFileChange={handleFileChange} />}></InputFileCard>
             <CommonTitleCard title="Parsed Resumes">
-             
+
                 {/* Jobs List in table format loaded from slice after api call */}
                 <ParseResumeTable parsed_resumes={resumes}></ParseResumeTable>
-                <div className="divider"></div> 
+                <div className="divider"></div>
                 <Subtitle styleClass={'text-center'}>Upload Job Description And Get Sorted Resume List</Subtitle>
-                <JobDesInput handleSubmit={handleSortResume} textRef={jod_des_text}></JobDesInput>
+                <JobDesInput handleSubmit={handleSortResume} errorText={error} textRef={jod_des_text}></JobDesInput>
             </CommonTitleCard>
 
             <CommonTitleCard title="Sorted Resumes">
-             
+
                 <SortedResumeTable sorted_resumes={sortedResumes}></SortedResumeTable>
-                
+
             </CommonTitleCard>
         </>
 
